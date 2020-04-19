4b850e46a14b254a2b96df9673a3302c287e2b67
Nivelle Mendiola <mendiolanivelle@gmail.com>
2020-04-19 18:32:42 +0800

    [Flutter World Time] - Setting up pages

---
+++ lib/pages/Choose_Location.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Choose_Location extends StatefulWidget {
+  @override
+  _Choose_LocationState createState() => _Choose_LocationState();
+}
+
+class _Choose_LocationState extends State<Choose_Location> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text('Choose Location screen'),
+    );
+  }
+}

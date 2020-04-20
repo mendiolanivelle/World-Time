dcb42c5034b969f63607348d3fccc76f4e036615
Nivelle Mendiola <mendiolanivelle@gmail.com>
2020-04-20 13:34:33 +0800

    [Flutter World Time App] - Asynchronous code

--- lib/pages/Choose_Location.dart
+++ lib/pages/Choose_Location.dart
@@ -8,11 +8,34 @@ class Choose_Location extends StatefulWidget {
 class _Choose_LocationState extends State<Choose_Location> {
 
   int counter = 0;
+  String name = 'Nivelle Mendiola';
+  String register = 'No';
+
+  void getData() async  
+  {
+    int counternumber = await Future.delayed(Duration(seconds: 5),() 
+    {
+      return counter = 100;     
+    });
+
+    String namechange = await Future.delayed(Duration(seconds: 2),()
+    {
+      return name = 'Zhenaica Lucero';
+    });
+
+    setState(() {
+      counter = counternumber;
+      name = namechange;
+    });
+    
+  }
 
   @override
   void initState() {
     super.initState();
     print('Init state function run');
+    getData();
+    register = 'Yes';
   }
 
   @override
@@ -31,6 +54,7 @@ class _Choose_LocationState extends State<Choose_Location> {
       (
         children: <Widget>
         [
+          Text('$name'),
           RaisedButton
           (
             onPressed: () 
@@ -41,6 +65,7 @@ class _Choose_LocationState extends State<Choose_Location> {
             },
             child: Text('the counter is : $counter'),
           ),
+          Text('$register'),
         ],
       ),
     );
